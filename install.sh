19c6808799f6a00d6443a6851e7b2362b67b23a2
gustavoriverag <gustavo.rivera@ug.uchile.cl>
2025-06-26 00:46:36 -0400

    More fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-ln -sf /src/logger-server.py /usr/local/bin/logger-server.py
-mkdir -p -v /var/local/logger-server 
+#!/bin/bash -x
+
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+ln -sf "$PWD"/src/logger-server.py /usr/local/bin/
+mkdir -p -v /var/local/temp-logger 
 chmod +x /usr/local/bin/logger-server.py
-cp /src/logger-server.service /etc/systemd/system/logger-server.service
+cp ./src/logger.service /etc/systemd/system/logger-server.service
 systemctl enable logger-server.service
 
 service logger-server restart
